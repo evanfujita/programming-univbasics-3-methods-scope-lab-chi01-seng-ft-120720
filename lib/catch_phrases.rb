62325ece3e88eca820ad2c07ea6ff48a426c2620
Evan Fujita <evanfujita@gmail.com>
2020-11-03 18:29:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,14 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  zelda = "It's Dangerous To Go Alone! Take This."
+  puts zelda
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
+end
